528069ffa199826d72ee5135ae00ffa88f5d6c55
Renata Kim <renata_kim@hotmail.com>
2022-06-22 15:23:21 -0300

    Primeira parte do exercicio

---
+++ Aula14/Administracao.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula14
+{
+    public abstract class Administracao
+    {
+        public abstract void Cadastrar();
+        public abstract void Remover();
+        public virtual void AtualizarCadastro()
+        {
+
+        }
+        public virtual void ListarTodosOsInscritos()
+        {
+
+        }
+    }
+}
